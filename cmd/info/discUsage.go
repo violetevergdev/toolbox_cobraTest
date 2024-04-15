5764cbe5cac06340ce9a916f48577af17a9eb5e4
VioletEverg <belabelllil@gmail.com>
2024-04-15 15:15:03 +0300

    first commit

---
+++ cmd/info/discUsage.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package info
+
+import (
+	"fmt"
+	"github.com/ricochet2200/go-disk-usage/du"
+	"github.com/spf13/cobra"
+)
+
+// discUsageCmd represents the discUsage command
+var discUsageCmd = &cobra.Command{
+	Use:   "discUsage",
+	Short: "Print disk usage of the current directory",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		usage := du.NewDiskUsage(".")
+		fmt.Printf("%v\n", usage)
+	},
+}
+
+func init() {
+	InfoCmd.AddCommand(discUsageCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// discUsageCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// discUsageCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
